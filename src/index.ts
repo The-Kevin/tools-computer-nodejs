15973a6d4af67c7a032824945ea3729fdec97023
the-kevin <kevin.almeida1111.ka@gmail.com>
2021-07-30 20:38:59 -0300

    feat: export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import * as network from './network/network';
+import * as cpu from './hardware/cpu';
+import * as crypto from './crypto/crypto';
 
-export { network };
+export { network, cpu, crypto };
